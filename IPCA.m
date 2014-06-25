f8053d39da67341404ca5a9161aa0ce47e0bcbb2
fkiraly <f.kiraly@ucl.ac.uk>
2014-06-25 13:58:37 +0100

    added IPCA implementation

---
+++ IPCA.m
@@ -0,0 +1,177 @@
+function IPCAresult = IPCA(X,IPCAparams)
+%IPCA       Ideal PCA
+%
+%usage
+%  [optional:
+%  IPCAparams.Z = ...
+%  IPCAparams.kernelparams = struct(...);
+%  ...       ]
+%
+%  IPCAresult = IPCA(X,params)
+%
+%input
+%  X          an N x n matrix of N training points
+%              (N points in n-space)
+%
+% optional, but recommended:
+%  IPCAparams    a struct with any subset of the following fields:
+%
+%    M               the number of feature spanning points
+%                     default is n^2
+%    Z               an M x n matrix which contains the basis for the feature space
+%                     default is randn(M x n)
+%    thresholdtype   a string which specifies the kind of thresholding in the SVD
+%                     'eps': all singular values >= threshold are retained
+%                     'num': the first threshold singular values are retained,
+%                            but only those which are bigger-than-machineeps
+%                     other/default: all bigger-than-machineeps are retained
+%    threshold       the singular value threshold
+%                     thresholdtype = 'eps': default is 0.01
+%                     thresholdtype = 'num': default is 5
+%    machineeps      the threshold for machine zero
+%                     default is 1e-12
+%    kernelparams    a struct or parameters for createKernel (see there for format)
+%                     default is inhomogenous polynomial kernel of degree 2 with theta = 1/sqrt(2)
+%                     if kernelparams is changed, M should be adapted as well
+%
+%
+%output
+% a struct of matrices:
+%  U         an N x m matrix of m leading left principal kernel vectors
+%  V         an M x m matrix of m leading right principal kernel vectors
+%  S         an m-vector of leading singular values
+%  Z         an N x n matrix which contains the basis for the feature space
+%             if Z was provided in params, these are equal, otherwise Z is
+%                                                         randomly generated
+%  dims      a struct containing m,n,M,N
+%  kmats     a struct containing pre-comnputed kernel matrices for further use
+%             Kxz = k(X,Z), Kzz = K(Z,Z), Kzzinv = pinv(Kzz),
+%             Kzzinvsqrt = real(sqrtm(pinv(Kzz))), Kzzsqrt = real(sqrtm(Kzz))
+%             KzzV, KzzD eigendecomposition of Kzz, Kzz = KzzV*diag(KzzD)*KzzV'           
+%  kernelparams   the kernel parameters which were used above
+%  IPCAtime   the time needed to compute the matrices above
+%
+%
+%authors
+%  Franz Király <f.kiraly@ucl.ac.uk>
+%  Martin Kreuzer <martin.kreuzer@uni-passau.de>
+%  Louis Theran <theran@math.fu-berlin.de>
+%
+% Copyright (c) 2014, Franz J. Kiraly, Martin Kreuzer, Louis Theran
+% All rights reserved.
+% see LICENSE
+
+
+[N,n] = size(X);
+
+
+%-- initialization with default parameters start
+if ~exist('IPCAparams','var')
+    deg = 2;
+    thresholdtype = 'eps';
+    threshold = 0.01;
+    M = min(n^2,N);
+    Z = randn(M,n);
+    kernelparams = struct('ktype','polynomial','theta',1/sqrt(2),'p',2);
+else
+    
+    if ~isfield(IPCAparams,'thresholdtype')
+        thresholdtype = 'eps';
+    else
+        thresholdtype = IPCAparams.thresholdtype;
+    end
+    
+    if ~isfield(IPCAparams,'machineeps')
+        machineeps = 1e-12;
+    else
+        machineeps = params.machineeps;
+    end
+    
+    if ~isfield(IPCAparams,'threshold')
+        switch thresholdtype
+            case 'eps'
+                %threshold = 0.01;
+                threshold = machineeps;
+            case 'num'
+                threshold = 5;
+            otherwise
+                thresholdtype = 'eps';
+                threshold = machineeps;
+        end
+    else
+        threshold = IPCAparams.threshold;
+    end
+
+    if ~isfield(IPCAparams,'Z')
+        if ~isfield(IPCAparams,'M')
+            M = min(n^2,N);
+        else
+            M = IPCAparams.M;
+        end
+        Z = randn(M,n);
+    else
+        [M,nZ] = size(IPCAparams.Z);
+        if nZ ~= n
+            error('dimensions of X and Z do not match')
+        end
+        Z = IPCAparams.Z;
+    end
+    
+    if ~isfield(IPCAparams,'kernelparams')
+        kernelparams = struct('ktype','polynomial','theta',1/sqrt(2),'p',2);
+    else
+        kernelparams = IPCAparams.kernelparams;
+    end
+    
+end
+%-- end initialization with default parameters
+
+
+
+%-- IPCA = thresholded SVD on the cross-kernel
+
+Kxz = createKernel(X,Z,kernelparams);
+Kzz = createKernel(Z,Z,kernelparams);
+
+tic;
+
+[KzzV,KzzD] = eig(Kzz);
+Kzzinv = KzzV*pinv(KzzD)*KzzV';
+Kzzsqrt = KzzV*sqrt(KzzD)*KzzV';
+Kzzinvsqrt = KzzV*sqrt(pinv(KzzD))*KzzV';
+
+IPCAtime = toc;
+
+
+[U,S,V] = svd(Kxz*real(sqrtm(pinv(Kzz))));
+
+switch thresholdtype
+    case 'eps'
+        cutoff = sum(diag(S)>= threshold);
+    case 'num'
+         machinecutoff = sum(diag(S)>= machineeps);
+         [sSd,~] = size(diag(S));
+         cutoff = min([threshold,sSd,machinecutoff]);
+end
+
+IPCAresult = struct;
+IPCAresult.U = U(:,1:cutoff);
+IPCAresult.V = V(:,1:cutoff);
+IPCAresult.S = diag(S(1:cutoff,1:cutoff));
+IPCAresult.Sinv = diag(pinv(S(1:cutoff,1:cutoff)));
+IPCAresult.Z = Z;
+IPCAresult.dims.m = cutoff;
+IPCAresult.dims.n = n;
+IPCAresult.dims.M = M;
+IPCAresult.dims.N = N;
+IPCAresult.kmats.Kxz = Kxz;
+IPCAresult.kmats.Kzz = Kzz;
+IPCAresult.kmats.Kzzinv = Kzzinv;
+IPCAresult.kmats.Kzzsqrt = Kzzsqrt;
+IPCAresult.kmats.Kzzinvsqrt = Kzzinvsqrt;
+IPCAresult.kmats.KzzV = KzzV;
+IPCAresult.kmats.KzzD = diag(KzzD);
+IPCAresult.kernelparams = kernelparams;
+IPCAresult.IPCAtime = IPCAtime;
+
+end
---
+++ IPCA_eval.m
@@ -0,0 +1,277 @@
+function IPCA_evalresult = IPCA_eval(Xtest,IPCAresult,evalparams)
+%IPCA_eval    Ideal PCA model evaluation
+%
+%usage
+%  IPCAresult = IPCA(X,IPCAparams)
+%  IPCAeval = IPCA_eval(Xtest,IPCAresult,evalparams)
+%
+%input
+%  Xtest         an Ntest x n matrix of Ntest test points
+%                   (Ntest points in n-space)
+%  IPCAresult    output of IPCA, learnt on training data
+%
+% optional, but recommended:
+%  evalparams    a struct which determines which outputs are computed.
+%                true = output is computed; other/omitted: output is not computed
+%   possible output fields:
+%    Principal Components:
+%     rightProj     projection onto the right (Z-type) principal components
+%     rightProjN    projection onto the right (Z-type) principal components, 
+%                    divided by singular vector (whitened)
+%     leftProj      projection onto the left (X-type/ordinary kernel PCA) principal components
+%     leftProjN     projection onto the left (X-type/ordinary kernel PCA) principal components
+%                    divided by singular vector (whitened)
+%    Manifold Components/Dual Principal Components:
+%     rightDist     distance vector to data manifold
+%     rightDistSq   squared length of distance vector
+%     leftDist      distance vector to data manifold projected onto X-span
+%     leftDistSq    squared length of disgance vector
+%
+%   default: rightProjN = true, rightDist = true, all others are not computed
+%
+%
+%output
+% a struct of matrices IPCA_evalresult 
+% Which of these are computed is determined by evalparams.
+%  fields have the same name as evalparams, but contain matrices
+% default: only rightProjN, rightDist are computed
+%
+%    Principal Components:
+%     rightProj     an Ntest x m matrix of right principal projections
+%     rightProjN    an Ntest x m matrix of whitened right principal projections
+%     leftProj      an Ntest x m matrix of left principal projections
+%     leftProjN     an Ntest x m matrix of whitened left principal projections
+%    Manifold Components/Dual Principal Components:
+%     rightDist     an Ntest x m matrix of residual right projection vectors
+%     rightDistSq   an Ntext-vector of their squared lengths
+%     leftDist      an Ntest x m matrix of residual left projection vectors
+%     leftDistSq    an Ntext-vector of their squared lengths
+%
+%
+%authors
+%  Franz Király <f.kiraly@ucl.ac.uk>
+%  Martin Kreuzer <martin.kreuzer@uni-passau.de>
+%  Louis Theran <theran@math.fu-berlin.de>
+%
+% Copyright (c) 2014, Franz J. Kiraly, Martin Kreuzer, Louis Theran
+% All rights reserved.
+% see LICENSE
+
+[~,n] = size(Xtest);
+
+if n ~= IPCAresult.dims.n
+    error('dimensions of Xtest and X do not match')
+end
+
+
+%-- initialization with default parameters start
+if ~exist('evalparams','var')
+   rightProj = false;
+   rightProjN = true;
+   leftProj = false;
+   leftProjN = false;
+   rightDist = true;
+   leftDist = false;
+else
+    
+    if ~isfield(evalparams,'rightProj')
+        rightProj = false;
+    else
+        if ~islogical(evalparams.rightProj)
+            rightProj = false;
+        else
+            rightProj = evalparams.rightProj;
+        end        
+    end
+    
+    if ~isfield(evalparams,'rightProjN')
+        rightProjN = false;
+    else
+        if ~islogical(evalparams.rightProjN)
+            rightProjN = false;
+        else
+            rightProjN = evalparams.rightProjN;
+        end        
+    end
+
+    if ~isfield(evalparams,'leftProj')
+        leftProj = false;
+    else
+        if ~islogical(evalparams.leftProj)
+            leftProj = false;
+        else
+            leftProj = evalparams.leftProj;
+        end        
+    end
+        
+    if ~isfield(evalparams,'leftProjN')
+        leftProjN = false;
+    else
+        if ~islogical(evalparams.leftProjN)
+            leftProjN = false;
+        else
+            leftProjN = evalparams.leftProjN;
+        end        
+    end
+    
+    if ~isfield(evalparams,'rightDist')
+        rightDist = false;
+    else
+        if ~islogical(evalparams.rightDist)
+            rightDist = false;
+        else
+            rightDist = evalparams.rightDist;
+        end        
+    end
+    
+    if ~isfield(evalparams,'rightDistSq')
+        rightDistSq = false;
+    else
+        if ~islogical(evalparams.rightDistSq)
+            rightDistSq = false;
+        else
+            rightDistSq = evalparams.rightDistSq;
+        end        
+    end
+        
+    if ~isfield(evalparams,'leftDist')
+        leftDist = false;
+    else
+        if ~islogical(evalparams.leftDist)
+            leftDist = false;
+        else
+            leftDist = evalparams.leftDist;
+        end        
+    end
+    
+    if ~isfield(evalparams,'leftDistSq')
+        leftDistSq = false;
+    else
+        if ~islogical(evalparams.leftDistSq)
+            leftDistSq = false;
+        else
+            leftDistSq = evalparams.leftDistSq;
+        end        
+    end
+        
+end
+
+if rightProj + rightProjN + leftProj + leftProjN + leftDist + leftDistSq + rightDist + rightDistSq == 0
+   error('no outputs have been requested in evalparams, aborting computation'); 
+end
+%-- end initialization with default parameters
+
+
+
+%-- load the model estimation results from IPCA
+kernelparams = IPCAresult.kernelparams;
+
+IPCAtime = IPCAresult.IPCAtime;
+
+Z = IPCAresult.Z;
+
+m = IPCAresult.dims.m;
+M = IPCAresult.dims.M;
+N = IPCAresult.dims.N;
+
+U = IPCAresult.U;
+V = IPCAresult.V;
+S = diag(IPCAresult.S);
+Sinv = diag(IPCAresult.Sinv);
+
+Kxz = IPCAresult.kmats.Kxz;
+Kzz = IPCAresult.kmats.Kzz;
+Kzzinv = IPCAresult.kmats.Kzzinv;
+Kzzinvsqrt = IPCAresult.kmats.Kzzinvsqrt;
+
+
+%-- eval.IPCA
+IPCA_evalresult = struct;
+
+Kxtz = createKernel(Xtest,Z,kernelparams);
+%Kxtx = createKernel(Xtest,X,kernelparams);
+
+if rightProj
+    tic;
+    IPCA_evalresult.rightProj = Kxtz*V;
+    IPCA_evalresult.rightProj_time = toc + IPCAtime;
+end
+
+if rightProjN
+    if rightProj
+        tic;
+        IPCA_evalresult.rightProjN = IPCA_evalresult.rightProj*Sinv;
+        IPCA_evalresult.rightProjN_time = IPCA_evalresult.rightProj_time + toc + IPCAtime;
+    else
+        tic;
+        IPCA_evalresult.rightProjN = Kxtz*V*Sinv;
+        IPCA_evalresult.rightProjN_time = toc + IPCAtime;
+    end
+end
+
+if leftProj
+    tic;
+    IPCA_evalresult.leftProj = Kxtz*Kzzinv*Kxz'*U;
+    IPCA_evalresult.leftProj_time = toc + IPCAtime;
+end
+
+if leftProjN
+    if leftProj
+        tic;
+        IPCA_evalresult.leftProjN = IPCA_evalresult.leftProj*Sinv';
+        IPCA_evalresult.leftProjN_time = IPCA_evalresult.leftProj_time + toc + IPCAtime;
+    else
+        tic;
+        IPCA_evalresult.leftProjN = Kxtz*Kzzinv*Kxz'*U*Sinv';
+        %IPCA_evalresult.leftProjN = Kxtx*U*Sinv';
+        IPCA_evalresult.leftProjN_time = toc + IPCAtime;
+    end
+end
+
+if rightDist
+    tic;
+    PV = eye(M) - V*V';
+    rd = Kxtz*Kzzinvsqrt*PV;
+    IPCA_evalresult.rightDist = rd;
+    IPCA_evalresult.rightDist_time = toc;
+end
+
+if rightDistSq
+    if rightDist
+        tic;
+        rd = IPCA_evalresult.rightDist;
+        rdsq = sum(rd.^2,2);
+        IPCA_evalresult.rightDistSq = rdsq;
+        IPCA_evalresult.rightDistSq_time = toc + IPCA_evalresult.rightDist_time;
+    else
+        tic;
+        PV = eye(M) - V*V';
+        rd = Kxtz*Kzzinvsqrt*PV;
+        rdsq = sum(rd.^2,2);
+        IPCA_evalresult.rightDistSq = rdsq;
+        IPCA_evalresult.rightDistSq_time = toc;
+    end
+end
+
+
+if leftDist
+    tic;
+    PU = eye(N)-U*U';
+    IPCA_evalresult.leftDist = Kxtz*Kzzinv*Kxz'*PU;
+    IPCA_evalresult.leftDist_time = toc + IPCAtime;
+end
+
+if leftDistSq
+    if leftDist
+        tic;
+        IPCA_evalresult.leftDistSq = IPCA_evalresult.leftDist.*IPCA_evalresult.leftDist';
+        IPCA_evalresult.leftDistSq_time = IPCA_evalresult.leftDist_time + toc + IPCAtime;
+    else
+        tic;
+    	PU = eye(N)-U*U';
+        IPCA_evalresult.leftDistSq = Kxtz*Kzzinv*Kxz'*PU*Kxz*Kzzinv*Kxtz;
+        IPCA_evalresult.leftDistSq_time = toc + IPCAtime;
+    end
+end
+
+end
